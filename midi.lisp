5a18aefc8a7afa74645c5a46af976e953a4849fc
Janne Nykopp <newcup@iki.fi>
2024-05-06 01:36:13 +0300

    Tested with CCL. Fix MIDI handling to understand MIDI Running Status

--- midi.lisp
+++ midi.lisp
@@ -70,6 +70,10 @@ if there are no elements to read."
            (type raw-buffer buffer))
   (bt:with-lock-held ((raw-buffer-lock buffer))
     (with-slots (octet-array curr-start curr-end unread-size) buffer
+      (declare (type (array (unsigned-byte 8)) octet-array)
+               (type fixnum curr-start)
+               (type fixnum curr-end)
+               (type fixnum unread-size))
       (when (> unread-size 0)
         (prog1
             (if (> curr-end curr-start)
@@ -185,6 +189,14 @@ elements in correct order."
   ()
   (:documentation "A midi note-off event"))
 
+(declaim (type (or null (integer #x80 #xef)) *midi-running-status*))
+(defparameter *midi-running-status* nil
+  "Voice and Mode Messages set the receiver in Running Status mode, where
+status octets are not necessary if the status doesn't change. This
+will be set to nil if any non voice or mode message is received, or
+any System Exclusive or Common Status message is received. Real-time
+messages won't affect running status.")
+
 (defmethod print-object ((object note-midi-event) stream)
   "Print note midi event"
   (print-unreadable-object (object stream :type t)
@@ -192,8 +204,8 @@ elements in correct order."
       (format stream "midi key: ~a velocity: ~a channel: ~a" key velocity channel))))
 
 (defun midi-status-octet-p (octet)
-  "Check if OCTET is a midi status octet."
-  (> octet #b1111111))
+  "Return OCTET if OCTET is a midi status octet, else nil."
+  (and (> octet #b1111111) octet))
 
 (defmacro with-default-midi-data-checking ((num-of-args raw-data ptr) &body body)
   "Wrap BODY in code that tests that midi data from RAW-DATA starting
@@ -205,25 +217,26 @@ elements in correct order."
     `(let ((,num-of-args-gs ,num-of-args)
            (,raw-data-gs ,raw-data)
            (,ptr-gs ,ptr))
-       (if (< (- (length ,raw-data-gs) ,ptr-gs) (1+ ,num-of-args-gs))
+       (if (< (- (length ,raw-data-gs) ,ptr-gs) ,num-of-args-gs)
            ;; Insufficient data. TODO: Log.
            (values nil ,ptr-gs)
            (let ((err-pos
-                  (position-if 'midi-status-octet-p ,raw-data-gs :start (1+ ,ptr-gs)
-                               :end (+ ,ptr-gs 1 ,num-of-args-gs))))
+                   (position-if 'midi-status-octet-p ,raw-data-gs
+                                :start ,ptr-gs :end (+ ,ptr-gs ,num-of-args-gs))))
              (if err-pos
                  ;; Invalid data (status octet instead of data
                  ;; octet). TODO: Log.
                  (values nil err-pos)
                  ;; Data was ok.
-                 (values ,@body (+ ,ptr-gs 1 ,num-of-args-gs))))))))
+                 (values ,@body (+ ,ptr-gs ,num-of-args-gs))))))))
 
 (defun gen-chan-handler-ignoring-n-args (num-of-args)
-  "Creates a midi octet stream handler that just ignores status octet
-  and NUM-OF-ARGS octets."
-  (lambda (raw-data ptr)
-    ;; (declare (type (simple-array (unsigned-byte 8)) raw-data)
-    ;;          (type fixnum ptr))
+  "Creates a midi octet stream handler that just ignores NUM-OF-ARGS
+octets."
+  (lambda (status raw-data ptr)
+    (declare (type (simple-array (unsigned-byte 8)) raw-data)
+             (type fixnum ptr))
+    (setf *midi-running-status* status)
     (with-default-midi-data-checking (num-of-args raw-data ptr)
       nil)))
 
@@ -232,18 +245,19 @@ elements in correct order."
   (declare (type (unsigned-byte 8) octet))
   (logand octet #b00001111))
 
-(defun note-generic-handler (raw-data ptr note-on-p)
+(defun note-generic-handler (status raw-data ptr note-on-p)
   "Generic handler for note midi message. Create the event from
-RAW-DATA, offset by PTR. If NOTE-ON-P is non-nil and velocity is
-greater than 0, the instance will be note-on. (Some midi devices send note-on
-  with velocity of 0 instead of note-off.)
+STATUS (the status octet) and RAW-DATA, offset by PTR. RAW-DATA at PTR
+should start the data octets for the note. If NOTE-ON-P is non-nil and
+velocity is greater than 0, the instance will be note-on. (Some midi
+devices send note-on with velocity of 0 instead of note-off.)
 
 Return values consisting of the instance and number of octets
 handled."
   (with-default-midi-data-checking (2 raw-data ptr)
-    (let ((channel (status-octet-channel-number (aref raw-data ptr)))
-          (key (aref raw-data (+ ptr 1)))
-          (velocity (aref raw-data (+ ptr 2))))
+    (let ((channel (status-octet-channel-number status))
+          (key (aref raw-data ptr))
+          (velocity (aref raw-data (1+ ptr))))
       (declare (type (integer 0 127) key velocity)
                (type (integer 0 15) channel))
       (make-instance (if (and note-on-p (> velocity 0))
@@ -251,22 +265,26 @@ handled."
                          'note-off-midi-event)
                      :channel channel :key key :velocity velocity))))
 
-(defun note-on-handler (raw-data ptr)
+(defun note-on-handler (status raw-data ptr)
   "Create and return a note-on or note-off event. (Some midi devices
   send note-on with velocity of 0 instead of note-off.)"
-  (note-generic-handler raw-data ptr t))
+  (setf *midi-running-status* status)
+  (note-generic-handler status raw-data ptr t))
 
-(defun note-off-handler (raw-data ptr)
+(defun note-off-handler (status raw-data ptr)
   "Create and return a note-off event."
-  (note-generic-handler raw-data ptr nil))
+  (setf *midi-running-status* status)
+  (note-generic-handler status raw-data ptr nil))
 
-(defun sys-common-handler (raw-data ptr)
+(defun sys-common-handler (status raw-data ptr)
   "Handle a system common message. These come in various forms"
-  (error "Sys-common-handler not yet implemented (data: ~a ~a)" raw-data ptr))
+  (setf *midi-running-status* nil)
+  (error "Sys-common-handler not yet implemented (status: ~a; data: ~a ~a)"
+         status raw-data ptr))
 
-(defun sys-real-time-handler (raw-data ptr)
+(defun sys-real-time-handler (status raw-data ptr)
   "Handle a system real time message. Right now, they are ignored."
-  (declare (ignore raw-data))
+  (declare (ignore status raw-data))
   (values nil (1+ ptr)))
 
 (defparameter *midi-channel-status-handler-mapper*
@@ -291,43 +309,44 @@ handled."
   (or (cdr (assoc (ash status-octet -4) *midi-channel-status-handler-mapper*))
       (cdr (assoc (ash status-octet -3) *midi-system-status-handler-mapper*))))
 
-(defun octets-to-midi-event (raw-data ptr)
-  "Create one midi-event from RAW-DATA. If the RAW-DATA is
-  insufficient, return values nil and PTR. If RAW-DATA is invalid,
-  return values nil and PTR + number of octets so far handled. If
-  RAW-DATA defines an event, return an event instance and PTR + number
-  of octets handled. The event instance returned may be nil, if the
-  event was to be ignored."
-  (let ((handler (get-handler-for (aref raw-data ptr))))
-    (funcall handler raw-data ptr)))
+(defun octets-to-midi-event (status raw-data ptr)
+  "Create one midi-event from STATUS octet and RAW-DATA. If the RAW-DATA
+is insufficient, return values nil and PTR. If RAW-DATA is invalid,
+return values nil and PTR + number of octets so far handled. If
+RAW-DATA defines an event, return an event instance and PTR + number
+of octets handled. The event instance returned may be nil, if the
+event was to be ignored."
+  (let ((handler (get-handler-for status)))
+    (funcall handler status raw-data ptr)))
 
 (defun create-midi-events-from-octets (raw-data)
   "Create new midi events from raw octet data RAW-DATA (vector or
 nil). Raw data might contain incomplete event in the end. Returns
-multiple values: a list of events, ordered by sequence of arrival, and
-a vector containing remaining unhandled handled octets in the case
-there was an incomplete event at the end. Malformed data is ignored."
-  (if (and raw-data (> (length raw-data) 0))
-      (let* ((data-ptr
-              (position-if 'midi-status-octet-p raw-data)) ; Fast-forward to status octet.
-             (data-left (not (null data-ptr)))
-             events)
-        (loop :while (and data-left (< data-ptr (length raw-data)))
-           :do (multiple-value-bind (event new-ptr)
-                   (octets-to-midi-event raw-data data-ptr)
-                 (when event
-                   (push event events))
-                 (if (= new-ptr data-ptr)
-                     ;; The event remaining was incomplete.
-                     (setf data-left nil)
-                     ;; Again fast-forward to next status octet if such is
-                     ;; found. Ignore anything else, since it must be
-                     ;; rubbish. TODO: Log.
-                     (setf data-ptr
-                           (or (position-if 'midi-status-octet-p raw-data :start new-ptr)
-                               new-ptr)))))
-        (values (reverse events) (subseq raw-data data-ptr)))
-      (values nil nil)))
+multiple values: a list of events ordered by arrival time, earliest
+first, and a vector containing remaining unhandled handled octets in
+the case there was an incomplete event at the end. Malformed data is
+ignored."
+  (let ((ptr 0) (raw-data-len (length raw-data))
+        events)
+    (loop :with status := nil
+          :while (< ptr raw-data-len)
+          :do
+             ;; Seek to next status octet or continue running status
+             (loop :for candidate-status := (midi-status-octet-p (aref raw-data ptr))
+                   :for curr-status := (or candidate-status *midi-running-status*)
+                   :when (or candidate-status (not curr-status)) :do (incf ptr)
+                   :do (setf status curr-status)
+                   :until curr-status)
+             ;; Then create an event
+             (multiple-value-bind (event new-ptr)
+                 (octets-to-midi-event status raw-data ptr)
+               (when event (push event events))
+               (if (= new-ptr ptr)
+                   ;; The last event was incomplete, nothing more to do.
+                   (loop-finish)
+                   ;; Otherwise continue.
+                   (setf ptr new-ptr))))
+    (values (reverse events) (subseq raw-data (min ptr raw-data-len)))))
 
 #+5am
 (progn
@@ -335,22 +354,86 @@ there was an incomplete event at the end. Malformed data is ignored."
   (in-suite midi-tests))
 
 #+5am
+(test ignore-rubbish-data
+  "Should not blow up if there's data without status octet, but should
+skip to next status."
+  (let (*midi-running-status*)
+    (multiple-value-bind (events remaining-data)
+        (create-midi-events-from-octets
+         #(#b01000101 #b01100101 #b10000000 #b01000101 #b01100101))
+      (is (= (length events) 1))
+      (is (= (length remaining-data) 0))
+      (let ((e (car events)))
+        (is (eq (type-of e) 'note-off-midi-event))
+        (is (= (get-channel e) 0))
+        (is (= (get-key e) 69))
+        (is (= (get-velocity e) 101)))
+      (is (= *midi-running-status* #b10000000)))))
+
+#+5am
 (test note-on-test
   "Try handling of note-on, followed by incomplete input for
-note-off. Should produce note-on event and remaining data of 2
-octets."
+note-off. Should produce note-on event and remaining data of 1
+octet and running-value of note-off on channel 1."
   (multiple-value-bind (events remaining-data)
       (create-midi-events-from-octets
        #(#b10010000 #b01000101 #b01100101 #b10000000 #b01000101))
     (is (= (length events) 1))
-    (is (= (length remaining-data) 2))
-    (is (= (aref remaining-data 0) #b10000000))
-    (is (= (aref remaining-data 1) #b01000101))
+    (is (= (length remaining-data) 1))
+    (is (= (aref remaining-data 0) #b01000101))
     (let ((e (car events)))
       (is (eq (type-of e) 'note-on-midi-event))
       (is (= (get-channel e) 0))
       (is (= (get-key e) 69))
-      (is (= (get-velocity e) 101)))))
+      (is (= (get-velocity e) 101)))
+    (is (= *midi-running-status* #b10000000))))
+
+#+5am
+(test note-on-running-status-test
+  "Try handling of note-on, followed by other note-on using running
+status, following incomplete note-on. Should produce two note-on events and
+remaining data of 1 octet."
+  (multiple-value-bind (events remaining-data)
+      (create-midi-events-from-octets
+       #(#b10010001 #b01000101 #b01100101 #b01000110 #b01100100 #b01000111))
+    (is (= (length events) 2))
+    (is (= (length remaining-data) 1))
+    (is (= (aref remaining-data 0) #b01000111))
+    (let ((e (first events)))
+      (is (eq (type-of e) 'note-on-midi-event))
+      (is (= (get-channel e) 1))
+      (is (= (get-key e) 69))
+      (is (= (get-velocity e) 101)))
+    (let ((e (second events)))
+      (is (eq (type-of e) 'note-on-midi-event))
+      (is (= (get-channel e) 1))
+      (is (= (get-key e) 70))
+      (is (= (get-velocity e) 100)))
+    (is (= *midi-running-status* #b10010001))))
+
+#+5am
+(test note-on-continue-from-running-status-test
+  "Try handling of note-on when previous run had processed the running
+status, following incomplete note-on. Should produce two note-on
+events and remaining data of 1 octet."
+  (let ((*midi-running-status* #1=#b10010010))
+    (multiple-value-bind (events remaining-data)
+        (create-midi-events-from-octets
+         #(#b01000101 #b01100101 #b01000110 #b01100100 #b01000111))
+      (is (= (length events) 2))
+      (is (= (length remaining-data) 1))
+      (is (= (aref remaining-data 0) #b01000111))
+      (let ((e (first events)))
+        (is (eq (type-of e) 'note-on-midi-event))
+        (is (= (get-channel e) 2))
+        (is (= (get-key e) 69))
+        (is (= (get-velocity e) 101)))
+      (let ((e (second events)))
+        (is (eq (type-of e) 'note-on-midi-event))
+        (is (= (get-channel e) 2))
+        (is (= (get-key e) 70))
+        (is (= (get-velocity e) 100)))
+      (is (= *midi-running-status* #1#)))))
 
 #+5am
 (test note-on-off-test
@@ -369,7 +452,8 @@ octets."
       (is (eq (type-of e2) 'note-off-midi-event))
       (is (= (get-channel e2) 3))
       (is (= (get-key e2) 61))
-      (is (= (get-velocity e2) 121)))))
+      (is (= (get-velocity e2) 121)))
+    (is (= *midi-running-status* #x83))))
 
 #+5am
 (test note-off-by-on-event-and-active-sensing
@@ -386,7 +470,31 @@ eaten)."
       (is (eq (type-of e) 'note-off-midi-event))
       (is (= (get-channel e) 8))
       (is (= (get-key e) 112))
-      (is (= (get-velocity e) 0)))))
+      (is (= (get-velocity e) 0)))
+    (is (= *midi-running-status* #x98))))
+
+#+5am
+(test note-on-then-off-by-on-event-and-active-sensing
+  "Test that note-on event with velocity 0 with running status results in
+note-off event. Also see that active sensing octets are ignored (but
+data eaten)."
+  (multiple-value-bind (events remaining-data)
+      (create-midi-events-from-octets
+       ;; Three active sensing octets at end, should be ignored.
+       #(#x98 #x70 #x30 #x70 #x00 #xFE #xFE #xFE))
+    (is (= (length events) 2))
+    (is (or (null remaining-data) (= (length remaining-data) 0)))
+    (let ((e (first events)))
+      (is (eq (type-of e) 'note-on-midi-event))
+      (is (= (get-channel e) 8))
+      (is (= (get-key e) 112))
+      (is (= (get-velocity e) 48)))
+    (let ((e (second events)))
+      (is (eq (type-of e) 'note-off-midi-event))
+      (is (= (get-channel e) 8))
+      (is (= (get-key e) 112))
+      (is (= (get-velocity e) 0)))
+    (is (= *midi-running-status* #x98))))
 
 #+5am
 (test no-data-handling
